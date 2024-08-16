19e1e32e4d0d0fb75d77df2cdcb7b6309419e66d
TarWittaya <wittayathongjeen698@gmail.com>
2024-08-16 15:28:11 +0700

    switch to force dynamic mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-// export const dynamic = 'force-dynamic'
+export const dynamic = "force-dynamic";
 
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
